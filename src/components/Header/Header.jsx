2127b4d89630af12ac7157e9a9334452562110a3
Nikita <mrbushik1@gmail.com>
2022-07-05 22:04:06 +0300

    end header and adding fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,12 @@ function Header() {
   return (
     <>
     <header className='header'>
-    <nav className='header-navbar'>
+    <div className='header-navbar'>
         <a href='##'>Правила</a>
         <a href='##'>Тарифы</a>
         <a className='ddd' href='##'>Контакты</a>
-    </nav>
-    <div>
+    </div>
+    <div className='logo-rectangle'>
   <div className='logo-wrapper'>
   <Watch/>
    <h4>DOSTAVER</h4>
@@ -19,8 +19,8 @@ function Header() {
     </div>
     <div className='header-contacts__container'>
       <div className='header-info__container'>
-        <a href='tel:74994084770'>+7 499 408 47 70</a>
-        <p>ежедневно с 08:00 до 20:00</p>
+        <a className='number-link' href='tel:74994084770'>+7 499 408 47 70</a>
+        <p className='header-work__info'>ежедневно с 08:00 до 20:00</p>
       </div>
       <div className='header-sotial'>
         <div className='header-rectangle'>
