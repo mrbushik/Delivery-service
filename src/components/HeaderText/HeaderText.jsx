364800c22cfec641b5bd6989d11d6a14ce47ea83
Nikita <mrbushik1@gmail.com>
2022-07-06 11:12:30 +0300

    add fonts and header title

---
+++ src/components/HeaderText/HeaderText.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import'./headerText.scss'
+function headerText() {
+  return (
+    <section>
+        <div className="container">
+            <div className="header-title__container">
+            <h1 className="main-title">Курьерская доставка</h1>
+        <h2 className='header-subtitle'>для интернет-магазинов в пределах Москвы и МО</h2>
+        <p>Обеспечим вашему бизнесу доставку точно в срок и с удобной возможностью оплаты заказов для вас и ваших клиентов </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default headerText
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import HeaderText from '../components/HeaderText/HeaderText'
 
 function mainPage() {
   return (
     <>
     <Header/>
+    <HeaderText/>
     </>
   )
 }
