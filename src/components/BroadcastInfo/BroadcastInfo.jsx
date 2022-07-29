be9214da66a10830e0efd30de2286dc5c8a4f612
Nikita <mrbushik1@gmail.com>
2022-07-29 14:26:22 +0300

    ready broatcast section

--- src/components/BroadcastInfo/BroadcastInfo.jsx
+++ src/components/BroadcastInfo/BroadcastInfo.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './BroadcastInfo.scss'
 import {Box,infoIcon, Plus} from '../../icons'
+import Row from '../styles/Row/Row'
 
 function BroadcastInfo() {
   return (
-    <div>
+    <div className='broadcast-info__section'>
 <h2 className="broadcast-title">Способы передачи информации о заказах:</h2>
+<div className="broadcast-info">
+<Row textArray={["Как", " Табличкой в Эксель в любом формате","Обычным текстом"]}/>
+<Row textArray={["Куда", "Telegram","WhatsApp", "На электронную почту order@dostaver.com"]}/>
+</div>
     </div>
   )
 }
