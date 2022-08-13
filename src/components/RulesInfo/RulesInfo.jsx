76ff977bce1c1fe4089158551eb55a23b43eeb6a
Nikita <mrbushik1@gmail.com>
2022-08-13 16:33:39 +0300

    ready rules page

--- src/components/RulesInfo/RulesInfo.jsx
+++ src/components/RulesInfo/RulesInfo.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Money, Report } from '../../icons';
+import { Money, Report, Cloud } from '../../icons';
 import TextItem from '../TextItems/TextItem';
-
+import './RulesInfo.scss';
 function RulesInfo() {
   return (
     <div className="rules-info">
       <div className="container">
-        <TextItem
-          icon={<Money />}
-          title={'Выручка'}
-          subtitle={
-            'Мы вычтем из выручки наше вознаграждение согласно отчёту и отправим её на Ваш расчётный счет на следующий день после доставок'
-          }
-        />
+        <div className="rules-svg">
+          <Cloud />
+        </div>
+        <div className="rules-info__container">
+          <TextItem
+            icon={<Money />}
+            title={'Выручка'}
+            subtitle={
+              'Мы вычтем из выручки наше вознаграждение согласно отчёту и отправим её на Ваш расчётный счет на следующий день после доставок'
+            }
+          />
+          <TextItem
+            icon={<Report />}
+            title={'Отчет'}
+            subtitle={
+              'Вместе с выручкой мы отправим отчёт. В нём будет содержаться статистика и информация о доставках, выполненных в отчётный день'
+            }
+          />
+        </div>
       </div>
     </div>
   );
